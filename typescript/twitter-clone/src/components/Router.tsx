ece27bd1a0caa1975fde9ccb718ce730658bd017
parkikbum <pibum@naver.com>
2022-02-17 01:30:16 +0900

    [ADD] 라우터 설정 및 프로젝트 재설치

---
+++ typescript/twitter-clone/src/components/Router.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import {HashRouter as Router, Route, Routes} from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+
+const AppRouter = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
+    return(
+        <Router>
+            <Routes>
+                {isLoggedIn ? (
+                <>
+                <Route path="/" element={<Home/>}>
+                    {Home}
+                </Route>
+                </>) : (
+                    <Route path="/" element={<Auth/>}>
+                        {Auth}
+                    </Route>
+                )}
+            </Routes>
+        </Router>
+    )
+}
+
+export default AppRouter;
--- typescript/twitter-clone/src/firebase.tsx
+++ typescript/twitter-clone/src/firebase.tsx
@@ -1,14 +1,12 @@
-import {initializeApp} from "firebase/app";
-import {getAuth} from "firebase/auth";
+import * as firebase from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCjxoZAVmEANH6G6kSKHaFF0BKRsSy7RDU",
-    authDomain: "twitter-clone-6426d.firebaseapp.com",
-    projectId: "twitter-clone-6426d",
-    storageBucket: "twitter-clone-6426d.appspot.com",
-    messagingSenderId: "335952733261",
-    appId: "1:335952733261:web:05cd78047b1feaf982a804"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.REACT_APP_APP_ID
   };
-
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+  
+  export default firebase.initializeApp(firebaseConfig);
