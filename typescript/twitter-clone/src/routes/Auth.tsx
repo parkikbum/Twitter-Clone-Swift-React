c6f71d0854fa0c36c68013c62d616969c0cd6cc6
parkikbum <pibum@naver.com>
2022-02-19 21:37:15 +0900

    [FEAT] 회원가입 구현

--- typescript/twitter-clone/src/routes/Auth.tsx
+++ typescript/twitter-clone/src/routes/Auth.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "@firebase/auth";
+import { authService } from "../fBase";
 
 const Auth = () => {
 const [email, setEmail] = useState("");
 const [password, setPassword] = useState("");
+const [newAccount, setNewaccount]= useState(true);
 
 const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const {target: {name, value}} = event;
@@ -13,15 +16,28 @@ const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setPassword(value);
     }
 }
-const onSubmit = (event: any) => {
+const onSubmit = async(event: any) => {
     event.preventDefault();
+    try{
+        let data;
+        if (newAccount) {
+            data = await createUserWithEmailAndPassword(authService, email, password);
+        } else {
+            data = await signInWithEmailAndPassword(authService, email, password);
+        }
+        console.log(data);
+    } 
+    catch(error){
+    }
+    //회원가입시 로그인도 바로 가능
+    
 }
 return(
 <div>
     <form onSubmit={onSubmit}>
         <input name="email" type="email" placeholder="Email" value={email} onChange={onChange} />
         <input name="password" type="password" placeholder="Password" value={password} onChange={onChange} />
-        <input type="submit" value="Log In" required />
+        <input type="submit" value={newAccount ? "Create Account" : "Log In"} required />
     </form>
     <div>
         <button>Continue with Google</button>
