68d2ca98d5d16fad491fe4ac2a1a935c2983a596
parkikbum <pibum@naver.com>
2022-02-20 01:43:39 +0900

    [FEAT] 라우터 설정 변경

--- typescript/twitter-clone/src/components/App.tsx
+++ typescript/twitter-clone/src/components/App.tsx
@@ -1,14 +1,28 @@
-import React, { useState } from 'react';
-import { boolean } from 'yargs';
+import React, { useEffect, useState } from 'react';
 import AppRouter from './Router';
 import { authService } from '../fBase';
+import { getAuth, onAuthStateChanged } from '@firebase/auth';
+
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    onAuthStateChanged(getAuth(), (user) => {
+      console.log(user);
+      if(user) {
+        setIsLoggedIn(true);
+        const uid = user.uid;
+      } else{
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
   return (
     <>
-    <AppRouter isLoggedIn = {isLoggedIn}/>
-    <footer>&copy; {new Date().getFullYear()} Twitter-Clone {authService.currentUser}</footer>
+    {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing...."}
+    <footer>&copy; {new Date().getFullYear()} Twitter-Clone </footer>
     </>
     );
 }
