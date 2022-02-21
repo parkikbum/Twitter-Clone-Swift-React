1239b59c7e5353abcfcb831c536cbb4d35d40658
parkikbum <pibum@naver.com>
2022-02-21 21:12:44 +0900

    [FEAT] 로그아웃 구현

--- typescript/twitter-clone/src/components/Router.tsx
+++ typescript/twitter-clone/src/components/Router.tsx
@@ -1,7 +1,10 @@
+import { profile } from "console";
 import React, { useState } from "react";
 import {HashRouter as Router, Route, Routes} from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Navigation from "./Navigation";
+import Profile from "../routes/Profile";
 
 interface logged {
     isLoggedIn: any;
@@ -10,11 +13,15 @@ interface logged {
 const AppRouter = ({isLoggedIn}: logged) => {
     return(
         <Router>
+            {isLoggedIn && <Navigation />}
             <Routes>
                 {isLoggedIn ? (
                 <>
                 <Route path="/" element={<Home/>}>
                     {Home}
+                </Route>
+                <Route path="/Profile" element={<Profile/>}>
+                    {Profile}                
                 </Route>
                 </>) : (
                     <Route path="/" element={<Auth/>}>
--- typescript/twitter-clone/src/routes/Profile.tsx
+++ typescript/twitter-clone/src/routes/Profile.tsx
@@ -1,4 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { authService } from "../fBase";
 
-const Profile = () => <span>Profile</span>
+const Profile = () => {
+    const onLogOutClick = () => {
+        authService.signOut();
+        navigate("/");
+    };
+    const navigate = useNavigate();
+    return (
+        <>
+        <button onClick={onLogOutClick}>Log Out</button>
+        </>
+    )
+};
 export default Profile;
